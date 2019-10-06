c2777521616e0f9af8911884695b4c2b9c060d4a
allison cortez <allizetorc@gmail.com>
2019-10-06 19:01:27 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,14 @@
-# require modules here
+# require modules 
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library(path)
+  emoticons ={"get_meaning" => {}, "get_emoticon" => {}}
+  YAML.load_file(path).each do  |meaning, describe|
+    eng,jan = describe
+    emoticons["get_meaning"][jan] = meaning
+    emoticons["get_emoticon"][eng] = jan
+  end  
+  emoticons
 end
 
 def get_japanese_emoticon
